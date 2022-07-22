482d883413aede1edfd73c60526ab7b3010e198e
raki <mohammed.raki26@gmail.com>
2022-07-22 11:52:13 +0200

    init projet

---
+++ src/main/java/model/Personne.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Personne {
+	private String prenom;
+
+	public Personne(String prenom) {
+		super();
+		this.prenom = prenom;
+	}
+	
+
+	public void setNom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return prenom;
+	}
+
+}
---
+++ src/test/java/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+import model.Personne;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personne p = new Personne("Mohammed-yassine");
+	}
+
+}
